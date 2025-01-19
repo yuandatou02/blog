f98c9ec32a1bb9087b86bb324456dbbddd77ea3b
yuandatou <yuandatou02@gmail.com>
2025-01-19 23:18:14 +0800

    创建前端路由

--- blog-admin/src/router/index.ts
+++ blog-admin/src/router/index.ts
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
-
-export default router
+import {createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router';
+// 路由项目
+export const constantRoutes: RouteRecordRaw[] = [
+  {
+    path: '/login',
+    name: '登录',
+    component: () => import('@/views/login/index.vue'),
+    meta: {
+      hidden: true,
+    }
+  },
+]
+// 创建路由
+const router = createRouter(
+  {
+    history: createWebHistory(),
+    routes: constantRoutes,
+  }
+);
+export default router;
