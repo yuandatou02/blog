7325e0fe0bfe1c74776013c05d0d7d1c5befc792
yuandatou <yuandatou02@gmail.com>
2025-01-21 11:13:47 +0800

    后端完成退出登录操作

--- blog-api/src/main/java/com/huang/controller/UserController.java
+++ blog-api/src/main/java/com/huang/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.huang.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.huang.model.vo.Result;
 import com.huang.model.vo.request.LoginReq;
 import com.huang.service.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,17 @@ public class UserController {
     private UserService userService;
 
     /**
+     * 用户退出
+     *
+     * @return @return {@link String} 退出成功信息
+     */
+    @GetMapping("/logout")
+    public Result<String> logout() {
+        StpUtil.logout();
+        return Result.success("用户退出成功");
+    }
+
+    /**
      * 用户登录
      *
      * @param login 登录参数
@@ -29,6 +42,6 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result<String> login(@Validated @RequestBody LoginReq login) {
-        return Result.success("登录成功", userService.login(login));
+        return Result.success("用户登录成功", userService.login(login));
     }
 }
