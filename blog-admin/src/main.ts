b1e39c9369dd630edd0581cd18f89a637bfe0ae0
yuandatou <yuandatou02@gmail.com>
2025-01-23 16:22:38 +0800

    前端网站设置页面完成

--- blog-admin/src/main.ts
+++ blog-admin/src/main.ts
@@ -7,12 +7,22 @@ import router from './router'
 import '@/permission'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import SvgIcon from '@/components/SvgIcon/index.vue'
+import VMdEditor from '@kangc/v-md-editor'
+import '@kangc/v-md-editor/lib/plugins/emoji/emoji.css'
+import createEmojiPlugin from '@kangc/v-md-editor/lib/plugins/emoji/index'
+import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index'
+import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 import 'virtual:svg-icons-register'
+import Prism from 'prismjs'
 
 const app = createApp(App)
-
+VMdEditor.use(vuepressTheme, { Prism }).use(createEmojiPlugin()).use(createTodoListPlugin())
 app.use(createPinia())
 app.use(router)
+app.use(VMdEditor)
 app.component('svg-icon', SvgIcon)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
