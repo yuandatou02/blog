d873d1c23c8a43675041320d5a69586df6210c43
Ikaros <yuandatou02@gmail.com>
2025-08-04 00:27:26 +0800

    ✨ feat(main): 连接数据库

--- axum-blog/src/main.rs
+++ axum-blog/src/main.rs
@@ -1,5 +1,7 @@
+use std::time::Duration;
+
 use dotenvy::dotenv;
-use tracing::info;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 #[tokio::main]
 async fn main() {
@@ -12,12 +14,24 @@ async fn main() {
         )
         .init();
     // 连接数据库
-    connect_db();
+    connect_db().await.expect("数据库连接失败！");
     //
     println!("hello world");
 }
 
-fn connect_db() {
-    let connect_url = std::env::var("DATABASE_URL").ok();
-    info!("connect_url is {}", connect_url.unwrap());
+async fn connect_db() -> Result<DatabaseConnection, sea_orm::DbErr> {
+    // 获数据库连接地址
+    let connect_url = std::env::var("DATABASE_URL").expect("数据库地址没有配置！");
+    // 创建数据库连接池
+    let mut opt = ConnectOptions::new(connect_url);
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(3600))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+    // 建立连接并返回
+    Database::connect(opt).await
 }
