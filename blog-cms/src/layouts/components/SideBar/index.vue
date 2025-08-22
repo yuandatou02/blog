c0200f6bbf9aafa780e4e36f4a461a18ce3c1da8
Ikaros <651908384@qq.com>
2025-08-22 13:13:46 +0800

    feat(ui): 添加设置接口及侧边栏logo控制

--- blog-cms/src/layouts/components/SideBar/index.vue
+++ blog-cms/src/layouts/components/SideBar/index.vue
@@ -1,9 +1,20 @@
 <template>
+  <div :class="{ 'has-logo': showLogo }">
+    <!-- 网站Logo -->
+    <logo v-if="showLogo" :collapse="isCollapse"/>
 
+  </div>
 </template>
 
 <script setup lang="ts">
+import {computed} from "vue";
+import useStores from "@/stores";
+import Logo from "@/layouts/components/SideBar/logo.vue";
 
+const {app, setting} = useStores();
+
+const showLogo = computed(() => setting.sidebarLogo);
+const isCollapse = computed(() => app.isCollapse);
 </script>
 
 <style scoped>
--- blog-cms/src/stores/index.ts
+++ blog-cms/src/stores/index.ts
@@ -1,9 +1,11 @@
 import useUserStore from "@/stores/modules/user.ts";
 import useAppStore from "@/stores/modules/app.ts";
+import useSettingStore from "@/stores/modules/setting.ts";
 
 const useStores = () => ({
     user: useUserStore(),
     app: useAppStore(),
+    setting: useSettingStore(),
 });
 
 export default useStores;
