039847529ef59b8959b5c602567eb846388a74f5
Ikaros <651908384@qq.com>
2025-08-29 23:56:10 +0800

    feat(web): 新增友链管理功能

---
+++ blog-cms/src/api/friend/index.ts
@@ -0,0 +1,54 @@
+import type {AxiosPromise} from "axios";
+import type {Friend, FriendForm, FriendQuery} from "@/api/friend/types.ts";
+import type {PageResult, Result} from "@/model";
+import request from "@/utils/request.ts";
+
+/**
+ * 查看友链列表
+ * @param params 查询条件
+ * @returns 友链列表
+ */
+export function getFriendList(params: FriendQuery): AxiosPromise<Result<PageResult<Friend[]>>> {
+    return request({
+        url: "/admin/friend/list",
+        method: "get",
+        params,
+    });
+}
+
+/**
+ * 删除友链
+ * @param data 友链id集合
+ */
+export function deleteFriend(data: number[]): AxiosPromise<Result<null>> {
+    return request({
+        url: "/admin/friend/delete",
+        method: "delete",
+        data,
+    });
+}
+
+/**
+ * 添加友链
+ * @param data 友链信息
+ */
+export function addFriend(data: FriendForm): AxiosPromise<Result<null>> {
+    return request({
+        url: "/admin/friend/add",
+        method: "post",
+        data,
+    });
+}
+
+/**
+ * 修改友链
+ * @param data 友链信息
+ */
+export function updateFriend(data: FriendForm): AxiosPromise<Result<null>> {
+    return request({
+        url: "/admin/friend/update",
+        method: "put",
+        data,
+    });
+}
+
--- blog-cms/src/main.ts
+++ blog-cms/src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import SvgIcon from '@/components/SvgIcon/index.vue'
 import RightToolbar from '@/components/RightToolbar/index.vue'
+import Pagination from "@/components/Pagination/index.vue"
 import "element-plus/dist/index.css"
 import "virtual:svg-icons-register"
 import "@/permission";
@@ -17,4 +18,5 @@ app.use(pinia);
 app.use(router);
 app.component("RightToolbar", RightToolbar);
 app.component('SvgIcon', SvgIcon);
+app.component("Pagination", Pagination);
 app.mount('#app');
