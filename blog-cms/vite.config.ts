cade600e50c8db287e7227091a321a76a3d54ef0
yuandatou02 <yuandatou02@gmail.com>
2025-11-12 23:42:58 +0800

    feat(blog-cms): 添加 Element Plus 和相关依赖
    新添加了 Element Plus、axios、sass、unplugin-auto-import 和 unplugin-vue-components 等依赖库，并更新了 deno.lock 文件以反映这些变化。这些依赖库的引入将为项目提供更丰富的 UI 组件、HTTP 请求功能、Sass 支持以及自动化导入和组件注册功能。
    
    - 在 package.json 中添加了 `axios`, `element-plus`, `sass`, `unplugin-auto-import` 和 `unplugin-vue-components` 依赖。
    - 在 deno.lock 中更新了 `@vitejs/plugin-vue` 的版本，添加了 `sass` 和相关依赖的条目。
    - 在 vite.config.ts 中添加了 `unplugin-auto-import` 和 `unplugin-vue-components` 插件，并配置了 ElementPlusResolver。
    - 在 tsconfig.node.json 中添加了 `baseUrl` 和 `paths` 配置，以便更好地支持模块路径别名。
    - 新增了 App.vue 和 main.ts 文件，App.vue 包含一个简单的模板用于显示 "你好"，main.ts 用于创建和挂载 Vue 应用实例。

--- blog-cms/vite.config.ts
+++ blog-cms/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }),
+  Components({
+    resolvers: [ElementPlusResolver()],
+  }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
