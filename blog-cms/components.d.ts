ef4f85fc9589b6d801bab5752e786255ba9c770b
yuandatou02 <yuandatou02@gmail.com>
2025-11-28 07:15:19 +0800

    feat: 添加登录页面UI和背景图片

--- blog-cms/components.d.ts
+++ blog-cms/components.d.ts
@@ -11,7 +11,11 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInput: typeof import('element-plus/es')['ElInput']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- blog-cms/src/views/login/index.vue
+++ blog-cms/src/views/login/index.vue
@@ -1,5 +1,64 @@
 <template>
-    <h1>你好</h1>
+  <div class="login">
+    <el-form class="login-form">
+      <h3 class="title">博客后台管理系统</h3>
+      <!--      账号-->
+      <el-form-item>
+        <el-input type="text" size="large" placeholder="账号"></el-input>
+      </el-form-item>
+      <!--      密码-->
+      <el-form-item>
+        <el-input type="password" show-password size="large" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="large" style="width: 100%;">
+          <span>登录</span>
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <!--    底部-->
+    <div class="el-login-footer">
+      <span>Copyright © 2025 - {{ new Date().getFullYear() }} By Ikaros</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url("@/assets/images/login-bg.png");
+  background-size: cover;
+
+  .login-form {
+    border-radius: 6px;
+    background: #ffffff;
+    width: 400px;
+    padding: 25px 25px 5px 25px;
+
+    .title {
+      margin: 0 auto 30px auto;
+      text-align: center;
+      color: #707070;
+    }
+  }
+}
+
+.el-login-footer {
+  height: 40px;
+  line-height: 40px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  color: #ffffff;
+  font-family: Arial, sans-serif;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+
+</style>
