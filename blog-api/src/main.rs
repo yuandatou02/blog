f765df80b7229c9ebc4b8486919f7b6412fef158
yuandatou02 <yuandatou02@gmail.com>
2025-11-30 08:59:55 +0800

    feat(blog-api): 添加基于axum的web服务器实现

--- blog-api/src/main.rs
+++ blog-api/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "hello,world!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
