541eb5680336f54e2bcdaeb3fde2da7753270a32
yuandatou02 <yuandatou02@gmail.com>
2025-11-30 22:19:10 +0800

    feat(error): 添加404页面及路由配置

--- blog-cms/src/router/index.ts
+++ blog-cms/src/router/index.ts
@@ -1,10 +1,17 @@
-import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
+import {createRouter, createWebHashHistory, type RouteRecordRaw} from "vue-router";
 
 export const constantRoutes: RouteRecordRaw[] = [
     {
         path: "/login",
         name: "登录",
         component: () => import("@/views/login/index.vue"),
+        meta: {
+            hidden: true,
+        }
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        component: () => import("@/views/error/404.vue"),
         meta: {
             hidden: true,
         }
