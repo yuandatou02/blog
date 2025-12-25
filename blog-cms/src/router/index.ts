b313997d203bad3228ab254c27349053a27d3131
Ikaros <yuandatou02@gmail.com>
2025-12-25 10:51:05 +0800

    feat(cms): 实现登录页面和路由配置
    - 添加登录页面组件，包含账号密码输入和登录按钮
    - 配置路由系统，添加登录页面路由规则
    - 引入SCSS样式文件，设置全局样式和应用布局
    - 更新App.vue使用RouterView组件渲染路由视图
    - 在package.json中添加sass依赖支持样式编译
    - 配置编码设置文件添加UTF-8字符编码配置
    - 生成自动导入和组件类型声明文件

--- blog-cms/src/router/index.ts
+++ blog-cms/src/router/index.ts
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory, type RouteRecordRaw} from "vue-router";
+
+const constantRoutes: RouteRecordRaw[] = [
+    {
+        name: "登录",
+        path: "/login",
+        component: () => import("@/views/login/index.vue"),
+        meta: {hidden: true},
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: constantRoutes,
+});
 
-export default router
+export default router;
---
+++ blog-cms/src/views/login/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <el-form class="login-form">
+      <h3 class="login-title">博客后台管理系统</h3>
+      <!--      账号-->
+      <el-form-item>
+        <el-input/>
+      </el-form-item>
+      <!--      密码-->
+      <el-form-item>
+        <el-input/>
+      </el-form-item>
+      <!--      登录按钮-->
+      <el-form-item>
+        <el-button/>
+      </el-form-item>
+    </el-form>
+    <!--  底部  -->
+    <div class="el-login-footer">
+      <span>Copyright © 2022 - {{ new Date().getFullYear() }} By Ikaros</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url("@/assets/images/login-bg.png");
+  background-size: cover;
+
+  .login-form {
+    border-radius: 6px;
+    background: #fff;
+    width: 400px;
+    padding: 25px 25px 5px 25px;
+
+    .login-title {
+      margin: 0 auto 30px auto;
+      text-align: center;
+      color: #707070;
+    }
+  }
+}
+
+.el-login-footer {
+  height: 40px;
+  line-height: 40px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-family: Arial, serif;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+</style>
