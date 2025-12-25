fe93e98bd0ddbce2c272e37394a688a664a4eb74
Ikaros <yuandatou02@gmail.com>
2025-12-25 15:44:47 +0800

    feat(icon): 添加SVG图标组件和相关配置
    - 新增SvgIcon组件用于显示SVG图标
    - 集成vite-plugin-svg-icons插件支持SVG图标注册
    - 添加chart、link、monitor、redis-list、size、system等SVG图标文件
    - 在main.ts中注册SvgIcon全局组件
    - 更新components.d.ts类型定义包含SvgIcon组件
    - 配置vite.config.ts中的SVG图标插件选项
    - 添加项目字典和检查配置文件

---
+++ blog-cms/src/components/SvgIcon/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg aria-hidden="true" class="svg-icon"
+       :style="'width:'+size+';height:'+size">
+    <use :xlink:href="symbolId" :fill="color"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import {computed} from "vue";
+
+/**
+ * 图标组件的属性接口定义
+ * 定义了图标的前缀、样式类、颜色和尺寸等可配置属性
+ */
+interface Props {
+  /**
+   * 图标前缀字符串，用于标识图标类型或分类
+   */
+  prefix?: string;
+  /**
+   * 图标的CSS样式类，用于自定义图标外观
+   */
+  iconClass?: string;
+  /**
+   * 图标的颜色值，支持颜色名称或十六进制颜色码
+   */
+  color?: string;
+  /**
+   * 图标的尺寸大小，如 small、medium、large 或具体数值
+   */
+  size?: string;
+}
+
+
+const props = withDefaults(defineProps<Props>(), {
+  prefix: "icon",
+  size: "1rem",
+});
+const symbolId = computed(() => `#${props.prefix}-${props.iconClass}`);
+</script>
+
+<style scoped lang="scss">
+.svg-icon {
+  vertical-align: -0.25em;
+  overflow: hidden;
+  fill: currentColor;
+}
+</style>
--- blog-cms/src/main.ts
+++ blog-cms/src/main.ts
@@ -4,9 +4,12 @@ import {createPinia} from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import SvgIcon from "@/components/SvgIcon/index.vue";
+import "virtual:svg-icons-register";
 
 const app = createApp(App);
 
+app.component("svg-icon", SvgIcon);
 app.use(createPinia());
 app.use(router);
 
