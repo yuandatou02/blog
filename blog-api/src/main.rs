21b5a33ccb8c52245f8c029541dc1f5952553298
Ikaros <yuandatou02@gmail.com>
2026-02-09 23:42:48 +0800

    chore(deps): 更新项目依赖项并重新生成锁文件
    - 添加了 axum 框架及其相关依赖项用于Web服务开发
    - 集成了 dotenvy 环境变量管理库
    - 添加了 sqlx 数据库操作库支持
    - 引入了 time 时间处理库
    - 添加了 tokio 异步运行时支持
    - 集成 tracing 和 tracing-subscriber 日志追踪系统
    - 更新了多种底层依赖库以确保兼容性

--- blog-api/src/main.rs
+++ blog-api/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::Router;
+use axum::routing::get;
+use time::macros::format_description;
+use tracing_subscriber::fmt::time::LocalTime;
+
+#[tokio::main]
+async fn main() {
+    // 1. 读取.env配置文件
+    dotenvy::dotenv().expect("Failed to load .env file");
+    // 2. 初始化日志订阅器
+    let timer = LocalTime::new(format_description!(
+        "[year]年[month]月[day]日 [hour]:[minute]:[second]"
+    ));
+    tracing_subscriber::fmt()
+        .with_timer(timer)
+        .with_level(true)
+        .with_target(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    tracing::info!("日志服务启动成功！");
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
+
+    tracing::info!("🚀 Server starting on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
